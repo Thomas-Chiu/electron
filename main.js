edd2a7475acb09fa192812c6e894020f05243be0
ThomasChiu <ThomasChiu@hyenatek.com>
2022-02-24 17:50:18 +0800

    feat: BrowserWindow webPreferences

--- main.js
+++ main.js
@@ -1,10 +1,17 @@
 const { app, BrowserWindow } = require("electron");
 const { datadogLogs } = require("@datadog/browser-logs");
+const path = require("path");
 
 const createWindow = () => {
   const win = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: 720,
+    height: 480,
+    useContentSize: true,
+    webPreferences: {
+      enableRemoteModule: true,
+      devTools: true,
+      preload: path.join(__dirname, "preload.js"),
+    },
   });
   win.loadFile("./index.html");
 };
@@ -17,6 +24,25 @@ app.on("quit", () => {
   app.quit();
 });
 
+// electron-reloader setting
 try {
   require("electron-reloader")(module);
 } catch {}
+
+/**
+ * datadog log
+ */
+
+datadogLogs.init({
+  clientToken: "pubcd5f640151316f9c0c9c9da8da090479",
+  site: "datadoghq.com",
+  // forwardErrorsToLogs: true,
+  sampleRate: 100,
+});
+
+console.log(datadogLogs);
+
+datadogLogs.logger.info("Button clicked", {
+  name: "buttonName",
+  id: 123,
+});
